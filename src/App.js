a1b886904bfb7917a10cbc4ee2250d0ae06d1c86
Bertil Tandayamo <bertilt4@gmail.com>
2020-09-28 16:12:04 -0500

    Fix linter issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-class App extends React.Component {
-    render() {
-        return <h1>This is a react app!</h1>
-    }
-}
+const App = () => (<h1>This is a react app!</h1>);
 
 export default App;
