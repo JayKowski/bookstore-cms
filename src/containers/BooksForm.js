151a28e332c0c0d29cb4aace4d5060c068e895db
JayKowski <jaymaina5@gmail.org>
2020-09-29 23:17:49 +0300

    connected to store, added state management for adding books

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,19 +1,34 @@
 /* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
+import { createBook } from '../actions/index';
+import { removeBook } from '../actions/index';
+import {connect} from 'react-redux';
 
 class BooksForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: '',
       title: '',
       category: '',
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    
+    if(this.state.title && this.state.category){
+      this.props.bookCreate(this.state);
+      this.setState({ id: "", title: "", category: "" }); 
+    }
+    e.target.reset();
+  }
 
   handleChange(e) {
-    if (e.target.name === 'title') { this.setState({ title: e.target.value }); }
+    const bookId = Math.floor(Math.random() * 100 + 1);
+    if (e.target.name === 'title') { this.setState({ id:bookId, title: e.target.value }); }
     else { this.setState({ category: e.target.value }); }
   }
 
@@ -21,7 +36,7 @@ class BooksForm extends Component {
     const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
     const { title, category } = this.state;
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label htmlFor="title">
           Book Title
           <input name="title" id="title" onChange={this.handleChange} />
@@ -29,12 +44,16 @@ class BooksForm extends Component {
         <br />
         <label htmlFor="categories">
           Categories
-          <select name="categories" id="categories" onChange={this.handleChange}>
-            {
-              categories.map((category, index) => (
-                <option key={`${category + index}`} value={category}>{category}</option>
-              ))
-            }
+          <select
+            name="categories"
+            id="categories"
+            onChange={this.handleChange}
+          >
+            {categories.map((category, index) => (
+              <option key={`${category + index}`} value={category}>
+                {category}
+              </option>
+            ))}
           </select>
         </label>
         <br />
@@ -44,4 +63,16 @@ class BooksForm extends Component {
   }
 }
 
-export default BooksForm;
+const mapDispatchToProps = dispatch => {
+      return {
+        bookCreate(book) {
+        dispatch(createBook(book));
+      },
+      removeBook(book) {
+        dispatch(removeBook(book));
+      }
+    }
+  
+  }
+
+export default connect(null, mapDispatchToProps)(BooksForm);
