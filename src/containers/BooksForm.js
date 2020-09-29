e5846c0119cf84b057bfc40cfba3a84e3729615c
Bertil Tandayamo <bertilt4@gmail.com>
2020-09-29 12:05:52 -0500

    Handle change method - The component should implement the handleChange method - Reacts to the user typing to input a name or to changes in the  category select.
    Co-authored-by: JayKowski <jaymaina5@gmail.com>

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,29 +1,47 @@
-import React from 'react';
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react';
 
-const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+class BooksForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      category: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-function BooksForm() {
-  return (
-    <form>
-      <label htmlFor="title">
-        Book Title
-        <input name="title" id="title" />
-      </label>
-      <br />
-      <label htmlFor="categories">
-        Categories
-        <select name="categories" id="categories">
-          {
-            categories.map(category => (
-              <option key={category.id} value={category}>{category}</option>
-            ))
-          }
-        </select>
-      </label>
-      <br />
-      <input type="submit" />
-    </form>
-  );
+
+  handleChange(e) {
+    if (e.target.name === 'title') { this.setState({ title: e.target.value }); }
+    else { this.setState({ category: e.target.value }); }
+  }
+
+  render() {
+    const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+    const { title, category } = this.state;
+    return (
+      <form>
+        <label htmlFor="title">
+          Book Title
+          <input name="title" id="title" onChange={this.handleChange} />
+        </label>
+        <br />
+        <label htmlFor="categories">
+          Categories
+          <select name="categories" id="categories" onChange={this.handleChange}>
+            {
+              categories.map((category, index) => (
+                <option key={`${category + index}`} value={category}>{category}</option>
+              ))
+            }
+          </select>
+        </label>
+        <br />
+        <input type="submit" />
+      </form>
+    );
+  }
 }
 
 export default BooksForm;
