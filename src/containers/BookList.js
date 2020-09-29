7ccd6a62205c21c1943900060d7dc836118cc79a
JayKowski <jaymaina5@gmail.org>
2020-09-29 23:16:51 +0300

    added click handler for remove action

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -2,9 +2,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
+import { removeBook } from "../actions/index";
 
-const BookList = ({ books }) => (
-  <div>
+const BookList = ({ books, bookRemove }) => {
+  
+  const handleRemove = id => {
+    console.log(id);
+    bookRemove(id);
+  };
+
+  return(
+    <div>
     <table>
       <thead>
         <tr>
@@ -21,16 +29,26 @@ const BookList = ({ books }) => (
               id={book.id}
               title={book.title}
               category={book.category}
+              handleClick={handleRemove}
             />
           ))
         }
       </tbody>
     </table>
   </div>
-);
+  );
+};
 
 const mapStateToProps = state => ({
   books: state.books,
 });
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    bookRemove(book) {
+      dispatch(removeBook(book));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList)
