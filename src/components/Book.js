6638044cffb97aa036cf32543495f51626566a82
JayKowski <jaymaina5@gmail.org>
2020-10-02 18:31:41 +0300

    imported css stylesheets file

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../book.css';
 
 const Book = ({
   id, title, category, handleClick,
 }) => (
-  <tr key={`book${id}`}>
-    <td>{id}</td>
-    <td>{title}</td>
-    <td>{category}</td>
-    <td><button type="button" onClick={() => handleClick(id)}> Delete </button></td>
-  </tr>
+  <div className="book-row" key={`book${id}`}>
+    <div className="book-specs">
+      <p className="category">{category}</p>
+      <p className="title">{title}</p>
+      <p className="author">Microverse</p>
+      <button className="links-btn" type="button">
+        comment
+      </button>
+      <button className="links-btn" type="button" onClick={() => handleClick(id)}>Delete</button>
+      <button className="links-btn" type="button">
+        edit
+      </button>
+    </div>
+    <div className="book-completion" />
+    <div className="book-chapters">
+      <p className="curr-chapt">current chapter</p>
+      <p className="chapter">chapter 17</p>
+      <button className="update-btn" type="button">update progress</button>
+    </div>
+  </div>
 );
 
 Book.propTypes = {
